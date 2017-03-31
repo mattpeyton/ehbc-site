9d78aa8c7242839c6de8d138435758d9a8b1e9f5
Matt Peyton <matthewcpeyton@gmail.com>
2017-03-30 21:29:29 -0400

    Added AJAX request to newsletter sign up

--- js/effects.js
+++ js/effects.js
@@ -3,7 +3,19 @@
    $(".navbar").fadeToggle();
  });
 
-//Newsletter sign up box
+$('#newButton').click(function() {
+  $overlay.append('<iframe class="newVid overlayContent" src="https://www.youtube.com/embed/Ex8Q1WWh2jo?ecver=1" frameborder="0" allowfullscreen></iframe>');
+  $overlay.show();
+  $overlay.click(function(e){
+    if (e.target !== this)
+      return;
+    $overlay.hide();
+    $(".overlayContent").remove();
+  });
+});
+
+
+//Newsletter sign up pop up window
 var $overlay = $('<div id="overlay"></div>');
 var $modWindow = $('<div class="overlayContent" id="modWindow"></div>');
 var $subForm = $('#subDiv');
@@ -11,17 +23,14 @@ var $nwsCloseX = $('#nwsCloseX');
 
 $("body").append($overlay);
 
-$('#newButton').click(function() {
-  $overlay.append('<iframe class="newVid overlayContent" width="560" height="315" src="https://www.youtube.com/embed/Ex8Q1WWh2jo?ecver=1" frameborder="0" allowfullscreen></iframe>');
-  $overlay.show();
-})
-
 //When Newsletter button is clicked, show overlay and popup
 $("#nwsltrButton").click(function(){
   $modWindow.append($subForm);
   $overlay.append($modWindow);
   $overlay.show();
   $subForm.show();
+
+  //Style color of selected field
   $(".nwsInput").focus(function(){
     $(this).css("background-color", "#ffffcc");
   });
@@ -29,51 +38,51 @@ $("#nwsltrButton").click(function(){
   $(".nwsInput").blur(function(){
     $(this).css("background-color", "#ffffff");
   });
+
+  //Close the pop up by clicking outside of the window...
+  $overlay.click(function(e){
+    if (e.target !== this)
+      return;
+    $overlay.hide();
+    $(".overlayContent").remove();
+  });
+
+  //...or close by clicking the exit box
   $nwsCloseX.click(function() {
     $overlay.hide();
     $(".overlayContent").remove();
   });
+
+  //submit Info from form to Mailchimp
+  $("#subForm").submit(function(evt){
+    evt.preventDefault();
+    var subData = {};
+    var url = $(this).attr("action");
+    subData.email_address = document.getElementById('nwsEmail').value;
+    subData.merge_fields = {};
+    subData.status = "pending";
+    subData.merge_fields.FNAME = document.getElementById('nwsFNAME').value;
+    subData.merge_fields.LNAME = document.getElementById('nwsLNAME').value;
+    subData = JSON.stringify(subData);
+    $.post(
+      url,
+      subData,
+      function(data){
+        $.parseJSON(data);
+        if (data.properties.status = "pending") {
+         $("#subForm").hide();
+          $("#subSuccess").show();
+        } else if (data.properties.status = "subscribed") {
+          $("#subForm").hide();
+          $("#subSubbed").show();
+        }
+      },
+      "json"
+    );
+    console.log(data);
+  });
  });
 
-//Activate colors in selected form input
-$(".nwsInput").focus(function(){
-  $(this).css("background-color", "#ffffcc");
-});
-
-$(".nwsInput").blur(function(){
-  $(this).css("background-color", "#ffffff");
-});
-
-//Close pop up
-///When overlay is clicked
-$overlay.click(function(e){
-  if (e.target !== this)
-    return;
-  $overlay.hide();
-  $(".overlayContent").remove();
-});
-
-///Or when exit box is clicked
-$nwsCloseX.click(function() {
-  $overlay.hide();
-  $(".overlayContent").hide();
-});
-
-//Submit name and email address
-function subscribeSubmit() {
-  var data = {};
-  data.email_address = document.getElementById('nwsEmail').value;
-  data.status = "pending";
-  data.merge_fields = {};
-  data.merge_fields.FNAME = document.getElementById('nwsFNAME').value;
-  data.merge_fields.LNAME = document.getElementById('nwsLNAME').value;
-  data = JSON.stringify(data);
-  console.log(data);
-};
-
-$(document).ready($("#nwsSubmitBtn").click(function(){
-  subscribeSubmit();
-}));
 
 //FAQ hide and show answers
 $(".FAnswer").hide();
